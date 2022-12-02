57d01eeb83abe8f165df923333ef91605f5e3e8a
Daniel Celestino <danielcelestino@gmail.com>
2022-12-02 13:55:48 -0300

    Including Dialog with contacts details

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ContatoService } from '../contato.service';
 import { Contato } from './contato';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+
+import { ContatoDetalheComponent } from '../contato-detalhe/contato-detalhe.component'
 
 @Component({
   selector: 'app-contato',
@@ -16,7 +19,8 @@ export class ContatoComponent implements OnInit {
 
   constructor(
     private service : ContatoService,
-    private fb : FormBuilder 
+    private fb : FormBuilder,
+    private dialog: MatDialog 
   ) { }
 
   ngOnInit(): void {
@@ -68,6 +72,14 @@ export class ContatoComponent implements OnInit {
       this.service.upload(contato, formData).subscribe(response => this.listarContatos());
     }
 
+  }
+
+  visualizarContato(contato:Contato){
+    this.dialog.open(ContatoDetalheComponent, {
+      width: '400px',
+      height: '450px',
+      data: contato
+    })
   }
 
 }
