fde2219715e43287e3079a84784786a902925f45
Daniel Celestino <danielcelestino@gmail.com>
2022-12-02 18:28:25 -0300

    Concluding order of conctacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,6 +41,7 @@ import { ContatoDetalheComponent } from './contato-detalhe/contato-detalhe.compo
     MatCardModule,
     MatDialogModule,
     MatPaginatorModule,
+    MatSnackBarModule,
     BrowserAnimationsModule
   ],
   providers: [
--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -6,6 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
 
 import { ContatoDetalheComponent } from '../contato-detalhe/contato-detalhe.component'
 import { PageEvent } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contato',
@@ -25,7 +26,8 @@ export class ContatoComponent implements OnInit {
   constructor(
     private service : ContatoService,
     private fb : FormBuilder,
-    private dialog: MatDialog 
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar 
   ) { }
 
   ngOnInit(): void {
@@ -61,8 +63,11 @@ export class ContatoComponent implements OnInit {
     const formValues = this.formularioContato.value;
     const contato : Contato = new Contato(formValues.nome, formValues.email);    
     this.service.salvar(contato).subscribe( resposta => {      
-      let lista: Contato[] = [... this.contatosList, resposta];
-      this.contatosList = lista;
+      this.listarContatos(this.pagina, this.tamanho);
+      this.snackBar.open('O Contato foi adicionado!', 'Sucesso!', {
+        duration:2000
+      });
+      this.formularioContato.reset();
     });
   }
 
